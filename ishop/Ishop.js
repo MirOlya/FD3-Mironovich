f3be65e62118dc0920763b18e4eed44b9d26d59b
MirOlya <ip.mironovich@gmail.com>
2021-09-14 14:50:51 +0300

    ishop v2

--- ishop/Ishop.js
+++ ishop/Ishop.js
@@ -15,20 +15,17 @@ const Ishop = React.createClass({
         this.props.companyGoods.forEach((el,i)=>{
             if(i===0){
                 arrGoods.push(React.DOM.tr({className:'Heading'},
-                React.DOM.td({className:'HeadNameGood'},el.name),
-                React.DOM.td({},
-                    React.DOM.img({className:'HeadPict',src:''+el.pict+''})),
-                React.DOM.td({className:'HeadRest'},el.rest),
-            )
-
+                React.DOM.th({className:'Head'},"Товар"),
+                React.DOM.th({className:'Head'},"Изображение"),
+                React.DOM.th({className:'Head'},"Остаток"),
+            ))
             };
             arrGoods.push(React.DOM.tr({key:el.code,className:'Npp'},
                 React.DOM.td({className:'NameGood'},el.name),
                 React.DOM.td({},
                     React.DOM.img({className:'Pict',src:''+el.pict+''})),
                 React.DOM.td({className:'Rest'},el.rest),
-            )
-        )
+            ))
            } );
       return React.DOM.div( {className:'Ishop'}, 
         React.DOM.div( {className:'Company'}, this.props.companyName ),
