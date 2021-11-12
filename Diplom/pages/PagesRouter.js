c891ca97716dcf8bd2926f93fd0819c8b7a4a1e2
MirOlya <ip.mironovich@gmail.com>
2021-11-12 18:22:35 +0300

    кое-что работает

--- Diplom/pages/PagesRouter.js
+++ Diplom/pages/PagesRouter.js
@@ -1,26 +1,24 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Route } from 'react-router-dom';
 
-import Page_About from './Page_About';
+import Page_Employees from './Page_Employees';
 import Page_Company from './Page_Company';
 import Page_Clients from './Page_Clients';
 import Page_Client from './Page_Client';
 
-class PagesRouter extends React.Component {
-          
-  render() {
+import './css/PagesRouter.css';
 
+function PagesRouter () {
     return (
-      <div>
-        <Route path="/" exact component={Page_About} />
+      <div className='PagesRouter'>
+        <Route path="/" exact component={Page_Employees} />
         <Route path="/company" component={Page_Company} />
         <Route path="/clients" component={Page_Clients} />
         <Route path="/client/:clid" component={Page_Client} />
       </div>
     );
     
-  }
 
 }
     
