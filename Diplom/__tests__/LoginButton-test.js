cf40fb361ae123dba184bdaaa6a03616ca0939b8
MirOlya <ip.mironovich@gmail.com>
2021-11-18 15:57:40 +0300

    подготовка для тестов

---
+++ Diplom/__tests__/LoginButton-test.js
@@ -0,0 +1,45 @@
+﻿"use strict";
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import FirstLogin from '../pages/Page_Login';
+
+test('работа Login', () => {
+
+  // создаём тестовую версию компонента
+  let companysArr =require('../companys.json');
+  let clientsArr=require('../clients.json');
+  
+  const component = renderer.create(
+    <FirstLogin/>
+  );
+
+  // получаем снэпшот (HTML-снимок) компонента для сверки, что вёрстка не испортилась
+  let componentTree=component.toJSON();
+  expect(componentTree).toMatchSnapshot();
+
+  // найдём в вёрстке компонента саму кнопку
+  const buttonElem = component.root.find( el =>  el.props.id==='loginButton' /*&& el.props.aaa == 'bbb'*/ ); 
+  // и "нажмём" на неё
+  buttonElem.props.onClick();
+
+  // получаем уже изменённый снэпшот
+  componentTree=component.toJSON();
+  expect(componentTree).toMatchSnapshot();
+
+  // "нажмём" кнопку ещё раз
+  buttonElem.props.onClick();
+  
+  // и получаем окончательный снэпшот
+  componentTree=component.toJSON();
+  expect(componentTree).toMatchSnapshot();
+  
+  /*
+  // можно эмулировать события, передавая в качестве объекта события то что нам нужно:
+  wrapper.find('select').simulate('change', {
+    target: { value: "hello" },
+  });
+  */
+
+});
